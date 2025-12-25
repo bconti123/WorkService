4974e3ca549e168cff1b912e0347859dea005855
bconti123 <bryantcconti@gmail.com>
2025-12-25 06:05:08 -0800

    Still fixing issues

--- WorkService.Api/Program.cs
+++ WorkService.Api/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using WorkService.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Missing connection string 'DefaultConnection'.");
+}
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
